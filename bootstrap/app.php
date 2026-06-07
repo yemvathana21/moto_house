77384ddc4588af5761fe7f39d67e7a5ba6d21d21
vathana <yemvathana86@gmail.com>
2026-06-07 15:21:05 +0700

    Clean up unused import in bootstrap/app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,8 +3,6 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Illuminate\Foundation\Testing\WithoutMiddleware;
-
 return Application::configure(basePath: dirname(__DIR__))
 
     ->withMiddleware(function (Middleware $middleware) {
