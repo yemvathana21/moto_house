41ec00c6eed78c8dac13c231c9881a7fbf7f1be8
vathana <yemvathana86@gmail.com>
2026-06-09 08:06:04 +0700

    Update

--- resources/views/components/layouts/store.blade.php
+++ resources/views/components/layouts/store.blade.php
@@ -210,7 +210,7 @@
             <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"/></svg>
         </a>
     </nav> -->
-
+    <!-- Notification Toast -->
     <div x-data="{ show: false, message: '' }"
          x-on:notify.window="message = $event.detail.message; show = true; setTimeout(() => show = false, 3000)"
          x-show="show"
