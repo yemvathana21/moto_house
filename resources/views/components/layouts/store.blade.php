c89a88f27410be266c50a47f250d371b6c702be1
vathana <yemvathana86@gmail.com>
2026-06-13 18:55:01 +0700

    fix: add relative positioning to wishlist badge

--- resources/views/components/layouts/store.blade.php
+++ resources/views/components/layouts/store.blade.php
@@ -62,7 +62,7 @@
                             </template>
                         </div>
                     </div>
-                    <a href="/wishlist" class="p-2.5 rounded-xl text-gray-500 hover:text-orange-600 hover:bg-orange-50 transition" title="{{ __('Wishlist') }}">
+                    <a href="/wishlist" class="relative p-2.5 rounded-xl text-gray-500 hover:text-orange-600 hover:bg-orange-50 transition" title="{{ __('Wishlist') }}">
                         <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"/>
                         </svg>
